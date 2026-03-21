a3572f88345f2247674f960a2a6cd2369077bc56
Lucas Rezende <lucas.rezende@southti.local>
2026-03-21 12:42:01 -0300

    Iniciando a automação dos testes de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spect.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test'
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  // Checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+
+  // Checkpoint
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-PVBGL5')
+
+  await page.getByTestId('search-order-id').click()
+
+  await expect(page.getByTestId('search-order-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-PVBGL5')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+})
