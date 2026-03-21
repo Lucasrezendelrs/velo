c2d482eb8f59a9866c8a51cc989e809b1bf7cd5b
Lucas Rezende <lucas.rezende@southti.local>
2026-03-21 12:54:47 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spect.ts
+++ playwright/e2e/pedidos.spect.ts
@@ -1,20 +1,17 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-PVBGL5')
-
   await page.getByTestId('search-order-id').click()
 
+  // Assert
   await expect(page.getByTestId('search-order-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-PVBGL5')
 
