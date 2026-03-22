7b52f09675e5b45dc80089309d54768aa322bd37
Lucas Rezende <lucas.rezende@southti.local>
2026-03-22 16:58:42 -0300

    Testando estados de erro e mensagens de feedback

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -8,6 +8,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
@@ -28,5 +29,38 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(containerPedido).toContainText(order, {timeout: 15_000})  
 
   await expect(page.getByText('APROVADO')).toBeVisible()
+
+})
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({page})=> {
+
+  // Teste Data
+  const order = 'VLO-ABC123'
+
+  // Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  // Act
+  await page.getByTestId('search-order-id').fill(order)
+  await page.getByTestId('search-order-button').click()
+
+  //await expect(page.locator('#root')).toContainText('Pedido não encontrado');
+  //await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente');
+
+  //const title = page.getByRole('heading', {name: 'Pedido não encontrado'})
+  //await expect(title).toBeVisible()
+
+  //const message = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+  //await expect(message).toBeVisible()
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
 
 })
