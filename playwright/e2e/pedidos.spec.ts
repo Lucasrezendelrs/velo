2043457b8ef94b10dc8684b6e5aa2d4c20404257
Lucas Rezende <lucas.rezende@southti.local>
2026-03-22 16:09:20 -0300

    Isolando dados de teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Teste Data
+  const order = 'VLO-PVBGL5'
+
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -8,7 +12,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-PVBGL5')
+  await page.getByTestId('search-order-id').fill(order)
   await page.getByTestId('search-order-button').click()
 
   // Assert
@@ -21,7 +25,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
     .filter({hasText: /^Pedido$/}) // /^Pedido$ Expressão regular que vai melhorar o critério de filtragem, buscando somente o tento que começa e termina com Pedido
     .locator('..') //Sobe para o elemento pai (a div que agrupa ambos)
 
-  await expect(containerPedido).toContainText('VLO-PVBGL5',{timeout: 15_000})  
+  await expect(containerPedido).toContainText(order, {timeout: 15_000})  
 
   await expect(page.getByText('APROVADO')).toBeVisible()
 
