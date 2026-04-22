5fe8792b09469acc36d0bc5f44fca88820146237
Lucas Rezende <lucas.rezende@southti.local>
2026-04-22 06:56:05 -0300

    Ajustando badge de status na consulta de pedidos e alinhando testes para EM_ANALISE

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,22 +3,23 @@ import { generateOrderCode } from '../support/helpers'
 
 test.describe('Consulta de Pedido', () => {
 
- 
-  
-  test.beforeEach(async ({page}) => {
-     // Arrange
-     await page.goto('http://localhost:5173/')
-     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
- 
-     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+
+  test.beforeEach(async ({ page }) => {
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   })
-  
+
   test('deve consultar um pedido aprovado', async ({ page }) => {
 
     // Teste Data
     const order = {
       number: 'VLO-PVBGL5',
+      status: 'APROVADO',
       color: 'Lunar White',
       wheels: 'sport Wheels',
       customer: {
@@ -37,8 +38,9 @@ test.describe('Consulta de Pedido', () => {
       - img
       - paragraph: Pedido
       - paragraph: ${order.number}
-      - img
-      - text: APROVADO
+      - status:
+        - img
+        - text: ${order.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
@@ -62,12 +64,21 @@ test.describe('Consulta de Pedido', () => {
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `);
 
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+
+    await expect(statusBadge).toHaveClass(/bg-green-100/)
+    await expect(statusBadge).toHaveClass(/text-green-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-circle-check-big/)
+
   })
   test('deve consultar um pedido reprovado', async ({ page }) => {
 
     // Teste Data
     const order = {
       number: 'VLO-LE2S4L',
+      status: 'REPROVADO',
       color: 'Midnight Black',
       wheels: 'sport Wheels',
       customer: {
@@ -86,8 +97,9 @@ test.describe('Consulta de Pedido', () => {
       - img
       - paragraph: Pedido
       - paragraph: ${order.number}
-      - img
-      - text: REPROVADO
+      - status:
+        - img
+        - text: ${order.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
@@ -110,6 +122,73 @@ test.describe('Consulta de Pedido', () => {
       - paragraph: ${order.payment}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `);
+
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status})
+
+    await expect(statusBadge).toHaveClass(/bg-red-100/)
+    await expect(statusBadge).toHaveClass(/text-red-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-circle-x/)
+
+  })
+  test('deve consultar um pedido em analise', async ({ page }) => {
+
+    // Teste Data
+    const order = {
+      number: 'VLO-QWEOAR',
+      status: 'EM_ANALISE',
+      color: 'Glacier Blue',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Maria Silva',
+        email: 'maria@velo.dev'
+      },
+      payment: 'À Vista'
+    }
+
+    // Act
+    await page.getByTestId('search-order-id').fill(order.number)
+    await page.getByTestId('search-order-button').click()
+
+    // Assert
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+
+    await expect(statusBadge).toHaveClass(/bg-amber-100/)
+    await expect(statusBadge).toHaveClass(/text-amber-700/)
+
+    const statusIcon = statusBadge.locator('svg')
+    await expect(statusIcon).toHaveClass(/lucide-clock3/)
 
   })
 
